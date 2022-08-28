071b6fdca641ab5194e0096b50073ac7f76a9e7b
zack wilder <zack.wilder@coinbase.com>
2022-08-28 18:36:19 -0400

    add create provider

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import Web3 from "web3";
+
+const advancedAPI = {
+	// Advanced API functions
+	test1: function (...params) {
+		return callNodeRPCMethod({
+			method: "node_rpc_test1",
+			params,
+		});
+	},
+
+	test2: async function (...params) {
+		return callNodeRestMethod({
+			method: "node_rest_test2",
+			params,
+		});
+	},
+};
+
+export function createNodeWeb3Provider({
+	username,
+	password,
+	endpoint,
+	...config
+}) {
+	const base64String = Buffer.from(`${username}:${password}`).toString(
+		"base64"
+	);
+	const options = {
+		keepAlive: true,
+		withCredentials: true,
+		timeout: 20000, // ms
+		headers: [{ name: "Authorization", value: "Basic " + base64String }],
+		...config,
+	};
+
+	const web3Provider = new Web3.providers.HttpProvider(endpoint, options);
+	const web3 = new Web3(web3Provider);
+
+	web3.node = {
+		...advancedAPI,
+		// others
+	};
+
+	return web3;
+}
+
+function callNodeRPCMethod({ method, params }) {
+	return Promise.resolve({ method, params });
+}
+
+async function callNodeRestMethod({ method, params }) {
+	const res = await fetch("https://www.example.com");
+	return Promise.resolve({ method, params, res });
+}
